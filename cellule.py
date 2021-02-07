6ca1bce79b53042dc95f16eade3bce03c603bd1b
Alexia <lexi.amoros@gmail.com>
2021-02-07 01:24:22 +0100

    main & grid & cellule file

---
+++ cellule.py
@@ -0,0 +1,12 @@
+class Cellule:
+    self.actuel = False
+    self.futur = False
+    voisins = []
+
+    def est_vivant(self):
+        return self.actuel
+
+    def set_voisins(self):
+
+    def get_voisins(self):
+        return voisins
