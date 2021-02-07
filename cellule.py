869c871480ae5b43a1d2af107acd8d4b08dcbae1
Alexia <lexi.amoros@gmail.com>
2021-02-07 01:57:26 +0100

    1

--- cellule.py
+++ cellule.py
@@ -1,7 +1,8 @@
 class Cellule:
-    self.actuel = False
-    self.futur = False
-    voisins = []
+    def __init__(self):
+        self.actuel = False
+        self.futur = False
+        voisins = []
 
     def est_vivant(self):
         return self.actuel
@@ -10,3 +11,20 @@ class Cellule:
 
     def get_voisins(self):
         return voisins
+
+    def naitre(self):
+        self.futur = True
+    
+    def mourrir(self):
+        self.futur = False
+
+    def basculer(self):
+        self.actuel = self.futur
+
+    def __str__(self):
+        if self.actuel=True:
+            return X
+        elif self.actuel=False:
+            return -
+
+    
