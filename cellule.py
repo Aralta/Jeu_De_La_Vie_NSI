55674eb2eedaaf91c6089402ef22305a973f63aa
Nicolas Gaido--Amoros <nicolas.gaidoamoros@viacesi.fr>
2021-02-11 02:49:10 +0100

    generate next cycle

--- cellule.py
+++ cellule.py
@@ -1,16 +1,41 @@
 class Cellule:
-    def __init__(self,state):
+    def __init__(self,grid,state,posx,posy):
+        #print(super().action_generate())
+        self.grid = grid
+        self.posx = posx
+        self.posy = posy
         self.actuel = state
         self.futur = False
-        voisins = []
+        self.alive_nearby = 0
+        #voisins = []
 
-    def est_vivant(self):
+    #Retourne l'état actuel de la cellule
+    def is_alive(self):
         return self.actuel
 
     # def set_voisins(self):
 
     def get_voisins(self):
-        return self.voisins
+        #print("get")
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+                nearbyx = self.posx + x
+                nearbyy = self.posy + y
+                
+                if nearbyx > 0 and nearbyy > 0 and nearbyx < self.grid.width and nearbyy < self.grid.height:
+                    nearby = self.grid.get_grid_info(nearbyx, nearbyy)
+                    if nearby == True:
+                        self.alive_nearby += 1
+                
+                if self.alive_nearby == 3 and self.actuel == False:
+                    self.naitre()
+                elif self.alive_nearby == range(2,3) and self.actuel == True:
+                    self.naitre()
+                else:
+                    self.mourrir()
+
+        #print("x :",self.posx ,"y :",self.posy,"alive_nei : ", self.alive_nearby)
+        #return self.voisins
 
     def naitre(self):
         self.futur = True
@@ -20,6 +45,7 @@ class Cellule:
 
     def basculer(self):
         self.actuel = self.futur
+        self.futur=False
 
     def __str__(self):
         if self.actuel==True:
--- grille.py
+++ grille.py
@@ -13,14 +13,14 @@ class Grille(tk.Frame):
         
 
         random = Random()
-        self.width = 10
-        self.height = 10
+        self.width = 15
+        self.height = 15
         self.grid = [[0 for i in range(self.width)] for x in range(self.height)]
 
         for x in range(self.width):
             for y in range(self.height):
                 rand = bool(random.getrandbits(1))
-                self.grid[x][y] = Cellule(rand)
+                self.grid[x][y] = Cellule(self,rand, x, y)
         self.create_widgets()
         #self.render_grid()
 
@@ -46,35 +46,45 @@ class Grille(tk.Frame):
         
         self.generate_gridframe()
         
-        generate = tk.Button(self.footer, text="generate", fg="grey",command=self.generate_gridframe).grid(row=0, column=0)
+        generate = tk.Button(self.footer, text="generate", fg="grey",
+                             command=self.action_generate).grid(row=0, column=0)
 
 
         purge = tk.Button(self.footer, text="purge", fg="grey",command=self.purge_grid).grid(row=0, column=1)
         quit = tk.Button(self.footer, text="QUIT", fg="grey",command=self.master.destroy).grid(row=0, column=2)
         
-
-     
-       
-        
+    
         
     def generate_gridframe(self):
         
-     
-        for widget in self.gridframe.winfo_children():
-            print(widget)
-            widget.destroy()
-        self.gridframe.grid_remove()
-        
-        
+        self.purge_grid()
+
         for x in range(self.width):
         
             for y in range(self.height):
                 celstate = self.grid[x][y].__str__()
                 frame = tk.Frame(self.gridframe, background=celstate, width=40, height=40, borderwidth=1).grid(row=x, column=y, padx=1, pady=1)
-
+    
+    #Purge the grid frame
     def purge_grid(self) : 
 
         for widget in self.gridframe.winfo_children():
-            print(widget)
             widget.destroy()
         self.gridframe.grid_remove()
+
+    def action_generate(self):
+
+        for x in range(self.width):
+            for y in range(self.height):
+                self.grid[x][y].get_voisins()
+        
+        for x in range(self.width):
+            for y in range(self.height):
+                self.grid[x][y].basculer()
+
+        self.generate_gridframe()
+
+    def get_grid_info(self,x,y):
+        return self.grid[x][y].is_alive()
+        
+       
